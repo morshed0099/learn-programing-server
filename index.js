100704875057bceff99bcc408168a9f7993b6e72
morshed0099 <mdgolammorshed0099@gmail.com>
2023-03-30 19:09:45 +0600

    my class route create and footer updated

--- index.js
+++ index.js
@@ -2,38 +2,63 @@ const express=require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
+
 app.use(cors())
+app.use(express.json())
 
-const coursesCategory = require('./data/courses-category.json')
-const course = require('./data/course.json')
+// const coursesCategory = require('./data/courses-category.json')
+// const course = require('./data/course.json')
 
-app.get('/category', (req, res) => {
-    res.send(coursesCategory);
-});
+console.log(process.env.DB_USER);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1m4kiwj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const courseCollection =client.db('learnProgramming').collection('courses')
+const categoryCollection=client.db('learnProgramming').collection('coursesCategory')
 
-app.get('/courses', (req, res) => {
-    res.send(course)
-})
-
-app.get('/', (req, res) => {
-    res.send('woow api running');
-});
-
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-    if (id == 07) {
-       res.send(course)
-    } else {
-        const select = course.find(ct => ct.id == id)
-        res.send(select) 
+async function run(){
+    try{
+        app.get('/category',async (req, res) => {
+            const query={}
+            const coursesCategory=await categoryCollection.find(query).toArray()
+            res.send(coursesCategory);
+            console.log(coursesCategory);
+        });
+        
+        app.get('/courses', async(req, res) => {
+            const query={}
+            const course=await courseCollection.find(query).toArray()
+            res.send(course)
+        })
+        
+        app.get('/', (req, res) => {
+            res.send('woow api running');
+        });
+        
+        app.get('/category/:id',async (req, res) => {
+            const id = req.params.id;
+            const course=await courseCollection.find({}).toArray()
+            if (id == 07) {               
+               res.send(course)
+            } else {        
+                const select = course.find(ct => ct.id == id)
+                res.send(select) 
+            }
+        })
+        
+        app.get('/course/:id',async(req,res)=>{
+            const id=req.params.id;
+            const course=await courseCollection.find({}).toArray()
+            const single=course.find(sc=>sc.id===id)
+            res.send(single)
+        })
     }
-})
+    finally{
 
-app.get('/course/:id',(req,res)=>{
-    const id=req.params.id;
-    const single=course.find(sc=>sc.id===id)
-    res.send(single)
-})
+    }
+}run().catch(error=>console.error(error))
 
 app.listen(port, () => {
     console.log(`woow ser is work on port ${port}`);
